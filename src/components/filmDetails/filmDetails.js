398cb65f03cb20fc40fe60da577c6563683ae0f7
Ivan Usenko <ivanja1994@gmail.com>
2020-08-12 22:06:59 +0300

    add footer

--- src/components/filmDetails/filmDetails.js
+++
@@ -1,7 +0,0 @@
-import hbsTemplate from './filmDetails.hbs';
-
-const markup = hbsTemplate();
-
-const filmDetails = document.querySelector('#main');
-
-filmDetails.insertAdjacentHTML('afterbegin', markup);
