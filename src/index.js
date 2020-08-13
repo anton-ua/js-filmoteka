f76182a3c861a7946e5859592823c07a3c95cada
Illya Kobyliatskii <flazzygardner1234@gmail.com>
2020-08-13 22:34:53 +0300

    fix conflict

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import './styles/normalize.scss';
 import './styles/styles.scss';
 
-<<<<<<< Updated upstream
 import './components/filmLibraryPage/libraryPage';
-import './components/header/header';
-import './components/footer/footer';
-import './components/filmDetails/filmDetails';
-=======
 import './components/header/header';
 import './components/footer/footer';
 import './components/filmDetails/filmDetails';
 
 import './main';
->>>>>>> Stashed changes
