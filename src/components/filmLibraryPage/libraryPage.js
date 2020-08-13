7e6a897161bc10a11431a7c6d3aa3a6e495c5695
Anton Isachenko <48572501+anton-ua@users.noreply.github.com>
2020-08-13 22:00:02 +0300

    Revert "Ivan chernovskiy"

--- src/components/filmLibraryPage/libraryPage.js
+++
@@ -1,6 +0,0 @@
-import films from './libraryPage.hbs';
-
-const ref = document.querySelector('#main');
-const markup = films();
-
-ref.insertAdjacentHTML('beforeend', markup);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
 import './styles/normalize.scss';
 import './styles/styles.scss';
-
-import './components/filmLibraryPage/libraryPage';
